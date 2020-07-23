7c83f2e46e4d93364e62597ff38056b35dce6c69
ssawyer0406 <ssawyer0406@gmail.com>
2020-07-23 03:28:37 -0500

    logic completed

--- logic.js
+++ logic.js
@@ -20,8 +20,7 @@
     otherwise return "red";
 */
 function getColor(number) {
-  // Uncomment and complete
-  return /* ( Your code Here ) ?  Your code Here  :  Your code Here */;
+  return number > 10 ? "blue" : "red";
 }
 
 /* 
@@ -50,7 +49,13 @@ console.log(getColor(10) === "red");
 */
 function lightStatus(brightness) {
   let result = "";
-  // Put your logic here
+  if (brightness == 0) {
+    result = "off";
+  } else if (brightness > 0 && brightness < 200) {
+    result = "dimmed";
+  } else if (brightness >= 200) {
+    result = "on";
+  }
   return result;
 }
 
@@ -88,14 +93,33 @@ console.log(lightStatus(255) === "on");
 
 function getLightBulbStatusDisplayString(status) {
   let result = "";
-  /* uncomment and complete
-    switch( your code here ) {
-      case "your code here": 
-        your code here;
-        break;
-      etc...
-    }
-    */
+  switch (status) {
+    case "on":
+      result = "The house is bright!";
+      break;
+    case "dimmed":
+      result = "The house is nice and dim";
+      break;
+    case "deleted":
+      result = "The lightbulb has been removed from the system";
+      break;
+    case "off":
+      result = "The house is dark";
+      break;
+    case "broken":
+      result = "The house is dark and we can't turn the light on!";
+      break;
+    case "offline":
+      result = "The house is dark and we can't find the lightbulb!";
+      break;
+    case "missing":
+      result = "The house is dark and we can't find the lightbulb!";
+      break;
+    default:
+      result = "Something is wrong!";
+      break;
+  }
+
   return result;
 }
 
@@ -214,7 +238,15 @@ function turnOffLight(lightName) {
 
 /* 
    -------YOUR CODE-----------------------------------------------------------
-*/
+//Implement this logic/*
+
+// If it's dark out, the porch light should be on.  If it's not dark, then it's off.
+// If nobody home, then turn all the lights off (except the porch light). 
+// If someone is home but they haven't gone to bed, then the living room and dining room lights should be on.
+// if someone is cooking, then the kitchen light should be on.
+// However, if they are watching TV, then turn off the livingroom and dining room lights.
+// If they are in bed, then the bedroom light should be on.
+// */
 function updateLights(
   somebodyIsHome,
   theyAreWatchingTV,
@@ -222,14 +254,42 @@ function updateLights(
   theyAreCooking,
   theyWentToBed
 ) {
-  // Write your code here!  You don't need to return anything, just call the given functions
-  // You should be using if else statements and the function arguments
-  // example of turning a light on
-  turnOnLight("livingRoomLight");
-  // example of turning off a light
-  turnOffLight("livingRoomLight");
+  if (!somebodyIsHome) {
+    turnOffLight("livingRoomLight");
+    turnOffLight("diningRoomLight");
+    turnOnLight("frontPorchLight");
+    turnOffLight("kitchenLight");
+    turnOffLight("bedroomLight");
+  } else {
+    if (!theyWentToBed) {
+      turnOnLight("livingRoomLight");
+      turnOnLight("diningRoomLight");
+    } else {
+      turnOnLight("bedroomLight");
+    }
+    if (theyAreCooking) {
+      turnOnLight("kitchenLight");
+    }
+    if (theyAreWatchingTV) {
+      turnOffLight("livingRoomLight");
+      turnOffLight("diningRoomLight");
+    }
+  }
+  if (itIsDarkOutside) {
+    turnOnLight("frontPorchLight");
+  } else {
+    turnOffLight("frontPorchLight");
+  }
 }
+// Implement this logic
 
+// If it's dark out, the porch light should be on.  If it's not dark, then it's off.
+// If nobody home, then turn all the lights off (except the porch light).
+// If someone is home but they haven't gone to bed, then the living room and dining room lights should be on.
+// if someone is cooking, then the kitchen light should be on.
+// However, if they are watching TV, then turn off the livingroom and dining room lights.
+// If they are in bed, then the bedroom light should be on.
+// */
 /* 
    -------TESTS---------------------------------------------------------------
    Run these commands to make sure you did it right. They should all be true.
